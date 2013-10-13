897650a96d307e3796a66249329a78a18a7e3c31
Adam Proctor <adam@uken.com>
2013-10-13 07:13:16 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pools::Application.config.secret_key_base = '793c60fabc88511a39b579055ed382bf65596a2aab49bde5675c9faceeb66542d1adcd0dc3e10286c1c3ffbc14207f988b0f87d991dde13f2fa16c4264935f00'
